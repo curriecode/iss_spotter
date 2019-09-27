3d70bfd421257e5468dc9d84f290b6a0f1db58bb
curriecode <melaniecurrie01@gmail.com>
2019-09-26 18:06:51 -0700

    "coordinates api implimemnted ns working"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // It will require and run our main fetch function.
 
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((err, ip) => {
   if (err) {
@@ -9,3 +9,12 @@ fetchMyIP((err, ip) => {
   }
   console.log('It worked! Returned IP:', ip);
 });
+
+fetchCoordsByIP('162.245.144.188', (err, ip) => {
+  if (err) {
+    console.log("It didn't work!", err);
+    return;
+  }
+  console.log('It worked! Returned: ', ip);
+});
+
--- iss.js
+++ iss.js
@@ -2,22 +2,43 @@
 // const args = process.argv.slice(2);
 const request = require('request');
 
-const fetchMyIP = function (callback) {
+const fetchMyIP = (callback) => {
   request('https://api.ipify.org?format=json', (err, response, body) => {
     if (err) {
       throw new Error("woops something is wrong with the request");
     }
-    if (err) {
-      callback(err, null);
-      return;
-    }
+    if (err) return callback(err, null);
+
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
-    const data = JSON.stringify(body);
-    callback(null, body)
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   });
 };
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://ipvigilante.com/${ip}`, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
